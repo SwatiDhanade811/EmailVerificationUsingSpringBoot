d80f0ba68496358c7c6dffbc1000c95fe51ea7c2
SwatiDhanade811 <96002384+SwatiDhanade811@users.noreply.github.com>
2023-09-24 17:57:54 +0530

    Add files via upload

---
+++ registration-with-email-verification-main/registration-with-email-verification-main/README.md
@@ -0,0 +1,5 @@
+# Spring boot user registration and email verification
+
+Refer to this article: https://deeppatel23.medium.com/user-registration-with-email-verification-in-java-and-spring-boot-699cb832ad2c
+
+
---
+++ registration-with-email-verification-main/registration-with-email-verification-main/src/main/java/com/example/registration/service/UserServiceImpl.java
@@ -0,0 +1,62 @@
+package com.example.registration.service;
+
+import com.example.registration.entity.ConfirmationToken;
+import com.example.registration.entity.User;
+import com.example.registration.repository.ConfirmationTokenRepository;
+import com.example.registration.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    ConfirmationTokenRepository confirmationTokenRepository;
+
+    @Autowired
+    EmailService emailService;
+
+    @Override
+    public ResponseEntity<?> saveUser(User user) {
+
+        if (userRepository.existsByUserEmail(user.getUserEmail())) {
+            return ResponseEntity.badRequest().body("Error: Email is already in use!");
+        }
+
+        userRepository.save(user);
+
+        ConfirmationToken confirmationToken = new ConfirmationToken(user);
+
+        confirmationTokenRepository.save(confirmationToken);
+
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(user.getUserEmail());
+        mailMessage.setSubject("Complete Registration!");
+        mailMessage.setText("To confirm your account, please click here : "
+                +"http://localhost:8088/confirm-account?token="+confirmationToken.getConfirmationToken());
+        emailService.sendEmail(mailMessage);
+
+        System.out.println("Confirmation Token: " + confirmationToken.getConfirmationToken());
+
+        return ResponseEntity.ok("Verify email by the link sent on your email address");
+    }
+
+    @Override
+    public ResponseEntity<?> confirmEmail(String confirmationToken) {
+        ConfirmationToken token = confirmationTokenRepository.findByConfirmationToken(confirmationToken);
+
+        if(token != null)
+        {
+            User user = userRepository.findByUserEmailIgnoreCase(token.getUserEntity().getUserEmail());
+            user.setEnabled(true);
+            userRepository.save(user);
+            return ResponseEntity.ok("Email verified successfully!");
+        }
+        return ResponseEntity.badRequest().body("Error: Couldn't verify email");
+    }
+}
